9ba5b0354ffe9a75ad73fe722e77c401bf67f877
Rebecca Turner <rbt@sent.as>
2020-05-14 14:20:07 -0400

    parser work; spans

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io::{self, Read};
 
+use nom::{error::VerboseError, IResult};
+
 use textecca::parse;
 
 fn main() -> io::Result<()> {
     let mut buf = String::new();
     io::stdin().read_to_string(&mut buf)?;
-    let tree = parse::parse(&buf).unwrap();
+    let tree: IResult<_, _, VerboseError<_>> = parse::parse(&buf);
     println!("{:#?}", tree);
     Ok(())
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,49 +1,78 @@
 use nom::{
     branch::alt,
-    bytes::complete::tag,
+    bytes::complete::{tag, take as take_bytes},
     character::complete::{char as take_char, none_of, one_of},
-    combinator::{all_consuming, map, not, recognize, rest_len, verify},
+    combinator::{all_consuming, complete, cut, map, not, opt, recognize, rest_len, verify},
+    error::{context, ParseError, VerboseError},
     multi::{many0, many1, many1_count, separated_nonempty_list},
     sequence::{pair, preceded, terminated, tuple},
     IResult,
 };
+use nom_locate::{position, LocatedSpan};
 
-/// Succeeds if there's no remaining input, fails otherwise.
-fn eof(i: &str) -> IResult<&str, &str> {
-    recognize(verify(rest_len, |len| *len == 0))(i)
+type Span<'input> = LocatedSpan<&'input str>;
+
+fn drop<'a, I, O, E, F>(f: F) -> impl Fn(I) -> IResult<I, (), E>
+where
+    I: Clone,
+    E: ParseError<I>,
+    F: Fn(I) -> IResult<I, O, E>,
+{
+    map(f, |_| ())
 }
 
-fn inline_whitespace(i: &str) -> IResult<&str, &str> {
+/// Succeeds if there's no remaining input, fails otherwise.
+fn eof<'a, E: ParseError<Span<'a>>>(i: Span<'a>) -> IResult<Span, (), E> {
+    not(take_bytes(1usize))(i)
+}
+
+fn inline_whitespace<'a, E: ParseError<Span<'a>>>(i: Span<'a>) -> IResult<Span, Span, E> {
     recognize(many1(one_of(" \t")))(i)
 }
 
-fn newline(i: &str) -> IResult<&str, &str> {
-    alt((tag("\n"), tag("\r\n")))(i)
+fn newline<'a, E: ParseError<Span<'a>>>(i: Span<'a>) -> IResult<Span, Span, E> {
+    // TODO: Only accept one style of line-ending per-file?
+    recognize(pair(opt(inline_whitespace), alt((tag("\n"), tag("\r\n")))))(i)
 }
 
-fn not_newline(i: &str) -> IResult<&str, char> {
+fn not_newline<'a, E: ParseError<Span<'a>>>(i: Span<'a>) -> IResult<Span, char, E> {
     // \r is only valid for newlines
     none_of("\r\n")(i)
 }
 
-fn paragraph_sep(i: &str) -> IResult<&str, usize> {
-    verify(many1_count(newline), |newlines| *newlines > 2)(i)
-}
-
-fn paragraph(i: &str) -> IResult<&str, &str> {
-    recognize(pair(
-        separated_nonempty_list(newline, many1(not_newline)),
-        alt((recognize(paragraph_sep), eof)),
-    ))(i)
+fn paragraph_sep<'a, E: ParseError<Span<'a>>>(i: Span<'a>) -> IResult<Span, Span, E> {
+    context(
+        "paragraph separator",
+        recognize(verify(many1_count(newline), |newlines| *newlines > 2)),
+    )(i)
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct ParseTree {
-    paragraphs: Vec<String>,
+pub struct Paragraph<'input> {
+    span: Span<'input>,
+    content: &'input str,
+    sep: Option<&'input str>,
 }
 
-pub fn parse(i: &str) -> IResult<&str, ParseTree> {
-    all_consuming(map(many0(paragraph), |paragraphs| ParseTree {
-        paragraphs: paragraphs.into_iter().map(String::from).collect(),
-    }))(i)
+fn paragraph<'a, E: ParseError<Span<'a>>>(i: Span<'a>) -> IResult<Span, Span, E> {
+    context(
+        "paragraph",
+        recognize(pair(
+            separated_nonempty_list(newline, many1(not_newline)),
+            alt((eof, drop(paragraph_sep))),
+        )),
+    )(i)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseTree<'input> {
+    paragraphs: Vec<Span<'input>>,
+}
+
+pub fn parse<'a, E: ParseError<Span<'a>>>(i: &'a str) -> IResult<Span, ParseTree, E> {
+    let i_span = Span::new(i);
+    // all_consuming(map(many0(paragraph), |paragraphs| ParseTree { paragraphs }))(i_span)
+    map(paragraph, |p| ParseTree {
+        paragraphs: vec![p],
+    })(i_span)
 }
